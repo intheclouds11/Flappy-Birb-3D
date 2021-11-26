4c5b981d77a7f320f87926547148e2399fb66c36
intheclouds11 <patrickmurr93@gmail.com>
2021-11-25 20:20:22 -0500

    Added flapBoost particles

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,6 +14,7 @@ public class Movement : MonoBehaviour
     private AudioSource audioSourceCollisions;
     [SerializeField] AudioClip flapTapSFX;
     [SerializeField] private AudioClip flapBoostSFX;
+    [SerializeField] private ParticleSystem flapBoostParticles;
     private Rigidbody rb;
 
     private bool hasFlapped = false;
@@ -56,6 +57,7 @@ public class Movement : MonoBehaviour
         {
             rb.AddRelativeForce(Vector3.up * boostAmount * Time.deltaTime);
             audioSourceMovement.clip = flapBoostSFX;
+            flapBoostParticles.Play();
 
             if (!audioSourceMovement.isPlaying)
             {
