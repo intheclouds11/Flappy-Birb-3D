1d8ab1f066a55562320ebae1a0f86cabce7aa23b
intheclouds11 <patrickmurr93@gmail.com>
2021-11-26 21:46:40 -0500

    expanding level 1

---
+++ Assets/Scripts/SpawnObstacle.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacle : MonoBehaviour
+{
+    private float currentGameTime = 0;
+    [SerializeField] float delayTime = 1f;
+
+    void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<CapsuleCollider>().enabled = false;
+    }
+
+    void Update()
+    {
+        Invoke("Spawn", delayTime);
+    }
+
+    private void Spawn()
+    {
+        GetComponent<MeshRenderer>().enabled = true;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<CapsuleCollider>().enabled = true;
+    }
+ 
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
